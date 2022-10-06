847c7fbd8d759f62c13d831bd4ad642f86b3c789
陈攀宇 <2965390478@qq.com>
2022-10-06 15:29:31 +0800

    m commit1

--- src/main/java/game/PhoneNumber.java
+++ src/main/java/game/PhoneNumber.java
@@ -3,7 +3,7 @@ package game;
 public class PhoneNumber {
     public static void main(String[] args) {
         int[] arr=new int[]{6,2,4,3,8,5,7,1,0,9};
-        int[] index=new int[]{2,5,1,3,4,0,2,6,4,5,2};
+        int[] index=new int[]{9,5,1,3,4,2,2,6,4,5,2};
         String tel="";
         for (int i:index)
         {
